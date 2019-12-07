1a7832df66f86d5c37244f7d00fbdd357241b174
chebb44 <chebb@yandex.ru>
2019-12-07 16:51:13 +0400

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let outStr = "";
+    let msgLength = expr.length / 10;
+    for (let i = 0; i < msgLength; i++) {
+        outStr += mesToCodeSymb(expr, i);
+    }
+
+    function mesToCodeSymb(expr, n) {
+
+        let currStr = "";
+        for (let i = (n * 10); i < (n * 10) + 10; i++) {
+            currStr += expr[i];
+        }
+        if (currStr == "**********") {
+            return (" ");
+        }
+        let decodeSymb = "";
+        let currLet = "";
+        for (i = 0; i < 10;) {
+            currLet = "";
+            currLet += currStr[i];
+            currLet += currStr[i + 1];
+            i += 2;
+            if (currLet === "10") {
+                decodeSymb += ".";
+            }
+            if (currLet === "11") {
+                decodeSymb += "-";
+            }
+        }
+        return (MORSE_TABLE[decodeSymb]);
+    }
+    return outStr;
 }
 
 module.exports = {
